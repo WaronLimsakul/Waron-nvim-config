ad5e2f0d70a1d5cd117c65d5572d1c8fdc8a57f6
Waron Limsakul <poonro@gmail.com>
2024-11-19 00:50:17 -0500

    add autopairs plugin and config lsp

--- lua/config/lsp_config.lua
+++ lua/config/lsp_config.lua
@@ -1,35 +1,45 @@
+require("fidget").setup()
 require("mason").setup()
 require("mason-lspconfig").setup()
 
-
 local lspconfig = require("lspconfig")
 local on_attach = function(_, _)
-	vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {}) -- rename everything you choose
-	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {}) -- rename everything you choose
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, {}) -- Go to definition
-	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {}) -- go to implementation
-	vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, {}) -- go to references
-	vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})                   -- Go to definition
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})               -- go to implementation
+    vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, {}) -- go to references
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 end
 local luasnip = require("luasnip")
 local cmp = require("cmp")
 local cmp_nvim_lsp = require('cmp_nvim_lsp')
 local capabilities = cmp_nvim_lsp.default_capabilities()
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
 cmp.setup({
-	sources = {
-		{name = 'nvim_lsp'},
-		{name = 'buffer'},
-		{name = 'path'},
-		{name = 'luasnip'},
-	},
-	snippet = {
-		expand = function(args)
-			luasnip.lsp_expand(args.body) -- Luasnip expands snippets
-		end,
-	},
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'buffer' },
+        { name = 'path' },
+        { name = 'luasnip' },
+    },
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body) -- Luasnip expands snippets
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({ -- try setting this to debug the auto completion
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+
+    }),
 })
-local servers = {"lua_ls", "html", "cssls", "ts_ls", "pylsp", "clangd", "omnisharp", "jdtls", "jsonls", "marksman", "eslint"}
+local servers = { "lua_ls", "html", "cssls", "ts_ls", "pylsp", "clangd", "omnisharp", "jdtls", "jsonls", "marksman",
+    "eslint", "tailwindcss" }
 for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup({
         on_attach = on_attach,
@@ -42,7 +52,7 @@ lspconfig.lua_ls.setup({
     settings = {
         Lua = {
             diagnostics = {
-                globals = {"vim"}, -- let lua_ls recognize vim in global scope, so no warning.
+                globals = { "vim" }, -- let lua_ls recognize vim in global scope, so no warning.
             },
         },
     },
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,10 +1,18 @@
 return {
-	{'neovim/nvim-lspconfig'},
-	{'hrsh7th/cmp-nvim-lsp'},
-	{'hrsh7th/nvim-cmp'},
-	{'williamboman/mason.nvim'},
-	{'williamboman/mason-lspconfig.nvim'},
-	{'hrsh7th/cmp-buffer'},
-	{'hrsh7th/cmp-path'},
-	{"L3MON4D3/LuaSnip"},
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "j-hui/fidget.nvim",
+    },
+    config = function()
+        require("config.lsp_config")
+    end
 }
