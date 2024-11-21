8a3f88fbf2a7235806ecf4b9236bd1913f3c0172
Waron Limsakul <poonro@gmail.com>
2024-11-20 22:24:17 -0500

    add colorizer and cmp-tailwind to work with tailwind

---
+++ lua/plugins/colorizer.lua
@@ -0,0 +1,8 @@
+return {
+    "NvChad/nvim-colorizer.lua",
+    event = "BufReadPre",
+    name = "colorizer",
+    opts = { user_default_options = {
+        tailwind = true,
+    }}
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,18 +1,28 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-        "j-hui/fidget.nvim",
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+            "j-hui/fidget.nvim",
+        },
+        config = function()
+            require("config.lsp_config")
+        end
     },
-    config = function()
-        require("config.lsp_config")
-    end
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = { { "roobert/tailwindcss-colorizer-cmp.nvim", config = true } },
+        opts = function(_, opts)
+            opts.formatting = {
+                format = require("tailwindcss-colorizer-cmp").formatter,
+            }
+        end
+    }
 }
